1d57bfc647ea3c18f30ffa72adb8ecf68c9c36d5
Sam Bhandari <sambhandari2021@yahooe.com>
2022-10-26 15:13:02 -0400

    create parseAndDisplayName function which find firstname and lastname individually

--- scripts/parsing.js
+++ scripts/parsing.js
@@ -1,1 +1,23 @@
-console.log('scripts works');
+// console.log('scripts works');
+
+
+// let fullName = "Sam Bhandari";
+// console.log(`My full name is ${fullName}`);
+// let indexOfSpace = fullName.indexOf(' ');
+// console.log(indexOfSpace);
+// let firstName = fullName.substring(0,indexOfSpace);
+// console.log(firstName);
+// let lastName = fullName.substring(indexOfSpace + 1);
+// console.log(lastName);
+
+
+function parseAndDisplayName(fullName) {
+    let indexOfSpace = fullName.indexOf(' ');
+    // console.log(indexOfSpace);
+    let firstName = fullName.substring(0,indexOfSpace);
+    // console.log(firstName);
+    let lastName = fullName.substring(indexOfSpace + 1);
+    console.log(firstName, lastName, fullName);
+
+}
+parseAndDisplayName('sam bhandari');
